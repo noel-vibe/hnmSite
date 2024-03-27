378cb563e117dfe8c19039d97cd297237fdb9866
noel-vibe <138129056+noel-vibe@users.noreply.github.com>
2024-03-27 23:52:31 +0900

    first commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
+
+const Navbar = () => {
+  const menuList = [
+    "여성",
+    "Divided",
+    "남성",
+    "신생아/유아",
+    "아동",
+    "H&M Home",
+    "Sale",
+    "지속가능성",
+  ];
+  return (
+    <div>
+      <div>
+        <div className="login-button">
+          <FontAwesomeIcon icon={faUser} />
+          <div>로그인</div>
+        </div>
+      </div>
+      <div className="nav-section">
+        <img
+          width={100}
+          src="https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FmPpwY%2FbtrS08qGYmD%2FHMrVGbYtNNpxynW3i53Zy1%2Fimg.png"
+        />
+      </div>
+      <div className="menu-area">
+          <ul className="menu-list">
+            {menuList.map(menu=><li>{menu}</li>)}
+          </ul>
+
+        <div>
+          <FontAwesomeIcon icon={faSearch} className="search-icon"/>
+          <input type="text" placeholder='Search' class='input-box' />
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/component/ProductCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <div>
+        <img width={200} src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Ff5%2F3b%2Ff53ba9cdc3e1d8e430e5fda9c3fd0247d7065d10.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]"/>
+        <div>Conscious choice</div>
+        <div>오버사이즈 티셔츠</div>
+        <div>₩14,900</div>
+        <div>신제품</div>
+      </div>
+        
+    </div>
+  )
+}
+
+export default ProductCard
